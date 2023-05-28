8342cfa8caa2619eaceb301d5f2ddf4fd3b4de60
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-05-28 00:11:21 -0400

    got the board game logic working

--- opening_complete/js/memory.js
+++ opening_complete/js/memory.js
@@ -1,14 +1,36 @@
-//mgStartBtn
+//store strings in an object
+const mgString = {
+    mgStartBtn : "mgStartBtn",
+    mgDirections : "mgDirections",
+    mgHomeScreen : "mgHomeScreen",
+    mgBoard : "mgBoard",
+    nodisplay : "nodisplay"
+}
+const setAttr = (domElement, attributeName, attributeValue) => {
+    domElement.setAttribute(attributeName , attributeValue);
+}
+const removeAttr = (domElement, attributeName) => {
+    domElement.removeAttribute(attributeName);
+}
 
-const mgStartBtn = getById("mgStartBtn");
-const mgDirections = getById("mgDirections");
-const mgHomeScreen = getById("mgHomeScreen")
+
+//selectors
+const mgStartBtn = getById(mgString.mgStartBtn);
+const mgDirections = getById(mgString.mgDirections);
+const mgHomeScreen = getById(mgString.mgHomeScreen)
+const mgBoard = getById(mgString.mgBoard)
 
 const showMGDirections = () => {
-    mgDirections.removeAttribute("nodisplay")
-    mgHomeScreen.setAttribute("mgHomeScreen")
+    // mgDirections.removeAttribute("nodisplay");
+    // console.log(mgDirections)
+    removeAttr(mgDirections, mgString.nodisplay);
+    // mgHomeScreen.setAttribute(mgString.mgHomeScreen);
+    setAttr(mgHomeScreen, mgString.nodisplay, "");
 }
 
 const memoryGameStart = () => {
-
+    console.log("Test function")
+    removeAttr(mgBoard, mgString.nodisplay);
+    setAttr(mgDirections, mgString.nodisplay, "");
 }
+
