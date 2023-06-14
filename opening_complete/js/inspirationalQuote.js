d01057797afb10c365ad3a279836314d9e4c3552
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-06-13 21:57:59 -0400

    correctly refactored the makeFetch function

--- opening_complete/js/inspirationalQuote.js
+++ opening_complete/js/inspirationalQuote.js
@@ -1,32 +1,37 @@
 // https://crudcrud.com/api/217f6bbc119e41fb9a72a5bd974ce316/<resource> 
 
-const apiKey = "217f6bbc119e41fb9a72a5bd974ce316";
+const apiKey = "edc7b25743dc4c089a0fe979610b0817";
 const resource = "people";
 const personId = "648517df456f2b03e80bca9b"
-const requestUrl = `https://crudcrud.com/api/${apiKey}/${resource}/${personId}`;
+const requestUrl = `https://crudcrud.com/api/${apiKey}/${resource}`;
 
 const body = {
-    name: "Josh",
+    name: "newName",
     skinColor: "dark"
 }
 
 const data = {
-    method: "GET", //states the request type of this data
+    method: "POST", //states the request type of this data
     headers: {
         'Accept': 'application/json', //type of data expected
         'Content-Type': 'application/json' //type of data sent
     },
-    // body: JSON.stringify(body)
+    body: JSON.stringify(body)
 }
-fetch(requestUrl, data).then(data => data.json().then(data => {
-    console.log(data);
-})).catch(err => console.log(err));
+
+
+// fetch(requestUrl, data).then(data => data.json().then(data => {
+//     console.log(data);
+// })).catch(err => console.log(err));
 
 
 //refactor the request function using async await
 
 async function makeFetch () {
-    const response = await fetch(requestUrl, data);
+    const response = await fetch(requestUrl,data);
     const dataToJSON = await response.json();
-    
+    console.log(dataToJSON);
 }
+
+
+makeFetch().catch(err => console.log(err));
